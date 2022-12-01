819e589b623c21fccebafcb816be9a87bff8f73a
ruturaj2001 <ravanruturaj29.com>
2022-12-01 23:50:08 +0530

    Welcome my git

---
+++ Calculator.java
@@ -0,0 +1,14 @@
+public class Calculator{
+	private int num1;
+	private int num2;
+    Calculator(int i,int j)
+	{
+		num1=i;
+		num2=j;
+	}
+	public  void printAdd()
+	{
+        System.out.println(num1+num2);
+	}
+     
+}
---
+++ CalculatorManager.java
@@ -0,0 +1,8 @@
+public class CalculatorManager{
+	public static void main(String []args)
+	{
+		Calculator c=new Calculator(20,30);
+		c.printAdd();
+	}
+     
+}
